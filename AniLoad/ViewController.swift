e5abe469259e0b20929f8b3f05f3db98f84b9b00
Bogdan Belogurov <belbog@me.com>
2019-02-12 20:45:24 +0300

    Initial Commit

---
+++ AniLoad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AniLoad
+//
+//  Created by Bogdan Belogurov on 12/02/2019.
+//  Copyright © 2019 Bogdan Belogurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
